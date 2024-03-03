5e5255058144c0ffa1ce5adde05e600d5e37af54
GEN <36332364+gntptrk@users.noreply.github.com>
2024-03-04 01:58:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.getDateAndTime = () => {
+    var today = new Date();
+    var date = today.getDate() + "-" + (today.getMonth()+1) + "-" +     today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    return dateTime
+}
